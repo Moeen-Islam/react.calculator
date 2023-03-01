c54bee1f1357f59e1e533186fb67c411c2fbbda8
Moeen-Islam <moeenislam8089@gmail.com>
2023-03-01 16:53:15 +0600

    Updates

---
+++ precache-manifest.36ccaf5ebe2ba26f9659d131f0c273c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2cde4948e6bab65345d",
+    "url": "/react.calculator/static/css/main.c22fda8a.chunk.css"
+  },
+  {
+    "revision": "f2cde4948e6bab65345d",
+    "url": "/react.calculator/static/js/main.f2cde494.chunk.js"
+  },
+  {
+    "revision": "766cdfe6be148af23e8f",
+    "url": "/react.calculator/static/js/1.766cdfe6.chunk.js"
+  },
+  {
+    "revision": "375f5cd91f78a60fe3a7",
+    "url": "/react.calculator/static/js/runtime~main.375f5cd9.js"
+  },
+  {
+    "revision": "7aa40bd2ed6b29a256d49ddd5f8569f8",
+    "url": "/react.calculator/index.html"
+  }
+];
